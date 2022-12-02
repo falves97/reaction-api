03d4127bdd9cecc428792b626f535873a952d3a8
Fernando Braga Alves <fbragaalves97@gmail.com>
2022-12-02 12:39:21 -0300

    feat: image uploud for user

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -22,6 +22,7 @@ use Symfony\Component\Validator\Constraints as Assert;
 #[ApiResource(
     normalizationContext: ['groups' => 'user:read'],
     denormalizationContext: ['groups' => 'user:write'],
+    validationContext: ['groups' => ['user:write']],
     processor: UserProcessor::class
 )]
 #[GetCollection]
@@ -29,13 +30,17 @@ use Symfony\Component\Validator\Constraints as Assert;
     uriTemplate: '/signup',
     openapiContext: [
         'security' => []
-    ]
+    ],
 )]
 #[Get]
-#[Put]
+#[Put(
+    denormalizationContext: ['groups' => ['user:write', 'user:update']]
+)]
 #[Delete]
-#[Patch]
-#[UniqueEntity('email')]
+#[Patch(
+    denormalizationContext: ['groups' => ['user:write', 'user:update']]
+)]
+#[UniqueEntity(fields: 'email', groups: ['user:write'])]
 class User implements UserInterface, PasswordAuthenticatedUserInterface
 {
     #[ORM\Id]
@@ -46,7 +51,7 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
 
     #[ORM\Column(length: 180, unique: true)]
     #[Groups(['user:read', 'user:write'])]
-    #[Assert\Email]
+    #[Assert\Email(['groups' => ['user:write']])]
     private ?string $email = null;
 
     #[ORM\Column]
@@ -64,13 +69,17 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
      *
      */
     #[Groups(['user:write'])]
-    #[Assert\NotBlank]
+    #[Assert\NotBlank(['groups' => ['user:write']])]
     private ?string $plainPassword = null;
 
     #[ORM\Column(length: 255)]
     #[Groups(['user:read', 'user:write'])]
-    #[Assert\NotBlank]
+    #[Assert\NotBlank(['groups' => ['user:write']])]
     private ?string $registration = null;
+
+    #[ORM\OneToOne(cascade: ['persist', 'remove'], orphanRemoval: true)]
+    #[Groups(['user:read', 'user:update'])]
+    private ?MediaObject $image = null;
 
     public function __construct()
     {
@@ -172,6 +181,18 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     public function setRegistration(string $registration): self
     {
         $this->registration = $registration;
+
+        return $this;
+    }
+
+    public function getImage(): ?MediaObject
+    {
+        return $this->image;
+    }
+
+    public function setImage(?MediaObject $image): self
+    {
+        $this->image = $image;
 
         return $this;
     }
